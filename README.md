a5e49cb1cb0218e7f440a7f828b74d3de87b1c00
lwenn <liww@dxy.cn>
2017-11-27 16:19:15 +0800

    init project

---
+++ README.md
@@ -0,0 +1,18 @@
+# vue-datetime-sample
+
+> A simple Vue.js project for testing fields of v`ue-datetime`
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+Then open the `dist/index.html` file.
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <datetime
+        v-model="date1"
+        type="date"
+        input-format="DD-MM-YYYY"
+        placeholder="Select date"
+        moment-locale="zh-cn">
+    </datetime>
+    <datetime
+        v-model="date2"
+        type="date"
+        input-format="DD-MM-YYYY"
+        placeholder="Select date"
+        locale="zh-cn">
+    </datetime>
+  </div>
+</template>
+
+<script>
+  import { Datetime } from 'vue-datetime'
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        date1: '',
+        date2: '',
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    components: {
+      Datetime
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
